fc69688915ccaf94e3a1738dd0ead0a96b260f46
Shihao01 <ysz_86@icloud.com>
2019-05-03 22:00:22 -0700

    Add files via upload

---
+++ js/msg.js
@@ -0,0 +1,9 @@
+// messages
+
+var SANSI_TOY_MSG = {
+	keyboard_hint: "You can use up down right left key to move.",
+	hello: "Hello?",
+	win: "YEAH~!<br /> Goal~！",
+	blind_alley: "Oh, died end！",
+	back_to_the_start: "why you back to start point？"
+};
